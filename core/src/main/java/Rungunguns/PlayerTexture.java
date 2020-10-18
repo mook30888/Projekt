414658dd48d94fc6910379c1dab308e23cdfc0c9
testuser-pleaseignore <test_user_please_ignore@hotmail.com>
2020-10-18 20:48:54 +0700

    Player Animation

--- core/src/main/java/Rungunguns/PlayerTexture.java
+++ core/src/main/java/Rungunguns/PlayerTexture.java
@@ -8,22 +8,24 @@ import org.mini2Dx.core.graphics.SpriteSheet;
 
 
 public class PlayerTexture {
-    private static final String PLAYER_SPRITE_SHEET_LOCATION = "Player/Player.png";
+    private static final String PLAYER_SPRITE_SHEET_LOCATION = "Mccree Running Sheet.png";
 
     private float frameDuration = 0.12f;
 
     Texture spriteTexture = new Texture(PLAYER_SPRITE_SHEET_LOCATION);
-    SpriteSheet playerSpriteSheet = new SpriteSheet(spriteTexture,55,70);
+    SpriteSheet playerSpriteSheet = new SpriteSheet(spriteTexture,134,139);
     Animation playerAnimation = new Animation();
 
 
     public PlayerTexture(){
         playerAnimation.addFrame(playerSpriteSheet.getSprite(0),frameDuration);
-       // playerAnimation.addFrame(playerSpriteSheet.getSprite(1),frameDuration);
+        playerAnimation.addFrame(playerSpriteSheet.getSprite(1),frameDuration);
         playerAnimation.addFrame(playerSpriteSheet.getSprite(2),frameDuration);
-      //  playerAnimation.addFrame(playerSpriteSheet.getSprite(3),frameDuration);
-      //  playerAnimation.addFrame(playerSpriteSheet.getSprite(4),frameDuration);
+        playerAnimation.addFrame(playerSpriteSheet.getSprite(3),frameDuration);
+        playerAnimation.addFrame(playerSpriteSheet.getSprite(4),frameDuration);
         playerAnimation.addFrame(playerSpriteSheet.getSprite(5),frameDuration);
+        playerAnimation.addFrame(playerSpriteSheet.getSprite(6),frameDuration);
+        playerAnimation.addFrame(playerSpriteSheet.getSprite(7),frameDuration);
     }
 
 }
--- core/src/main/java/Rungunguns/TopBottomEdgeTexture.java
+++ core/src/main/java/Rungunguns/TopBottomEdgeTexture.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class TopBottomEdgeTexture {
 
-    private static final String GROUND_TEXTURE_LOCATION = "groundDirt.png";
+    private static final String GROUND_TEXTURE_LOCATION = "Test Map.png";
 
     Texture groundTexture = new Texture(Gdx.files.internal(GROUND_TEXTURE_LOCATION));
 }
