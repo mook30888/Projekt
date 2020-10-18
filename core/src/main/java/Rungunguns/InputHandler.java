c6509e4f453732ef7ee1934bd8254996b7901452
mook <keettamook_k@cmu.ac.th>
2020-10-18 22:55:30 +0700

    เอ้าออกมาวิ่ง วิ่งนะๆ แฮมทาโร่

--- core/src/main/java/Rungunguns/InputHandler.java
+++ core/src/main/java/Rungunguns/InputHandler.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.Input;
 
 public class InputHandler {
 
-    boolean spacePressed() {
-        return Gdx.input.isKeyJustPressed(Input.Keys.SPACE);
-    }
+    boolean spacePressed() {return Gdx.input.isKeyJustPressed(Input.Keys.SPACE);}
+    boolean zPressed(){return  Gdx.input.isKeyJustPressed(Input.Keys.Z);}
+    boolean xPressed(){return  Gdx.input.isKeyJustPressed(Input.Keys.X);}
 }
